5f8f0e4b1018c483cd5e7d905f89029546ab81e8
Piotr Kuglin <kuglinpiotr@gmail.com>
2019-12-04 19:21:14 +0100

    logging introduction

--- Server.py
+++ Server.py
@@ -1,6 +1,4 @@
 import logging
-import time
-import threading
 import concurrent.futures
 import socket
 import random
@@ -8,21 +6,23 @@ import random
 MAX_CLIENT_LISTEN = 10
 MAX_CLIENT_LOGGED = 256000
 
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+c_handler = logging.FileHandler("log.log")
+c_handler.setLevel(logging.INFO)
+c_format = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+c_handler.setFormatter(c_format)
+logger.addHandler(c_handler)
+
 
 def receiver_module_execute(client_socket, client_address, client_id, thread_pool):
-    print("receiver")
-    print(client_socket)
-    print(client_address)
-    print(client_id)
-    print(thread_pool)
+    pass
+    logger.info("closing receiver module for client: %s", client_id)
 
 
 def sender_module_execute(client_socket, client_address, client_id, thread_pool):
-    print("sender")
-    print(client_socket)
-    print(client_address)
-    print(client_id)
-    print(thread_pool)
+    pass
+    logger.info("closing sender module for client: %s", client_id)
 
 
 class Server:
@@ -37,16 +37,20 @@ class Server:
                 return x
 
     def start(self):
-        logging.log()
+        logger.info("Server main thread has been started")
         main_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         main_socket.bind((socket.gethostbyname(socket.gethostname()), self.server_port))
         main_socket.listen(MAX_CLIENT_LISTEN)
+        logger.info("Server listen at port: %s", self.server_port)
 
         while True:
             (client_socket, client_address) = main_socket.accept()
+            logger.info("connection accepted from: %s , port: %s", client_address[0], client_address[1])
             client_id = self.random_client_id()
+            logger.info("assigned client_id: %s", client_id)
             self.thread_pool_executors_list[client_id] = concurrent.futures.ThreadPoolExecutor(2)
             client_executor = self.thread_pool_executors_list.get(client_id)
+            logger.info("creating modules for client: %s", client_id)
             client_executor.submit(receiver_module_execute, client_socket, client_address, client_id, self.thread_pool_executors_list)
             client_executor.submit(sender_module_execute, client_socket, client_address, client_id, self.thread_pool_executors_list)
 
