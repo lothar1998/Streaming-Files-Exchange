153c11b1ceade189a3cbedd26dbcabcd125f4b85
kamkali <kamkalis@gmail.com>
2019-12-05 18:19:50 +0100

    Trying ogarnac threading but troche not umiem

--- server/ServerReceiveModule.py
+++ server/ServerReceiveModule.py
@@ -1,21 +1,24 @@
 import socket
-from typing import Dict
+from typing import Dict, List
 import queue
 
 
 class ServerReceiveModule:
-    def __init__(self, client_id, clients_queue: Dict[str, queue.Queue]):
+    def __init__(self, client_id, clients_queue: Dict[str, List]):
         self.client_id = client_id
         self.clients_queue = clients_queue
+        self.thread_pool = None
+
+    def thread_queue_handler(self, other_cli_id):
+        self.clients_queue[f'{other_cli_id}'][4] = self.clients_queue[f'{self.client_id}'][4]
 
     def receive_message_form_client(self, client_socket: socket.socket):
         q = queue.Queue(maxsize=1024)
-        self.clients_queue[f'{self.client_id}'] = q
+        self.clients_queue[f'{self.client_id}'][4] = q
         data_msg = f'Receiving data from client: {self.client_id}'
         client_socket.send(bytes(data_msg, 'utf-8'))
 
         # Receive data in chunks
-        full_msg = ''
         buffer_size = 16
         while True:
             data = client_socket.recv(buffer_size)
@@ -24,12 +27,11 @@ class ServerReceiveModule:
             if not len(data):
                 return False
 
-            self.clients_queue[f'{self.client_id}'].put(data.decode('utf-8'))
-
             print(f"Received {data}")
 
+            self.clients_queue[f'{self.client_id}'][4].put(data.decode('utf-8'))
+
             if not data:
-                # to logger?
                 print(f"Received whole message from {client_socket}")
-                self.clients_queue[f'{self.client_id}'].put(None)
+                self.clients_queue[f'{self.client_id}'][4].put(None)
                 break
--- server/ServerSendModule.py
+++ server/ServerSendModule.py
@@ -1,12 +1,35 @@
+from typing import Dict, List
+import concurrent.futures
+
+
 class ServerSendModule:
-    def __init__(self, clients_list, client_id, server_ip, server_port=6969):
-        self.client_id = client_id
-        self.server_ip = server_ip
-        self.server_port = server_port
-        self.clients_list = clients_list
+    def __init__(self, ip_address, port, client_socket, clients_queue: Dict[str, List], client_address):
+        self.ip_address = ip_address
+        self.port = port
+        self.client_socket = client_socket
+        self.clients_queue = clients_queue
+        self.client_address = client_address
+        self.thread_pool = None
 
-    @staticmethod
-    def welcome_message_send(client_id, client_socket, client_address):
-        cli_info = f"Client with ip {client_address} and id: {client_id} has connected to the server"
+    def send_message_to_client(self, sender_id, receiver_id, file_to_send):
+        # set this client Busy
+        self.clients_queue[f'{self.clients_queue}'][3] = True
+        cli_info = f"Client with ip {self.client_address} and id: {sender_id} has connected to the server"
 
-        client_socket.send(bytes(cli_info, 'utf-8'))
+        self.client_socket.send(bytes(cli_info, 'utf-8'))
+
+        # check if client is busy
+        if self.clients_queue[f'{receiver_id}'][3]:
+            print(f"Client_{receiver_id} is Busy")
+            return False
+
+        # set sender_id and get queue
+        receiver_q = self.clients_queue[f'{receiver_id}'][4](sender_id)
+
+        receiver_q.put(file_to_send)
+
+        print("Loaded whole file bytes to queue")
+        receiver_q.put(None)
+
+        # Unlock this client
+        self.clients_queue[f'{self.clients_queue}'][3] = False
